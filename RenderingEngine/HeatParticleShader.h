0625ba43356a7ad4c1f7bf05111a93e6d0fcec14
JeremyAdamHart <jeremyadamhart@gmail.com>
2017-10-02 18:09:24 -0600

    Sorry Andrew

--- RenderingEngine/HeatParticleShader.h
+++ RenderingEngine/HeatParticleShader.h
@@ -12,9 +12,9 @@ protected:
 	void calculateUniformLocations();
 	void loadUniforms(const glm::mat4& vp_matrix, const glm::mat4& inv_rot_matrix);
 public:
-	HeatParticleShader(std::map<GLenum, std::string> defines = std::map<GLenum, std::string> defines{});
+	HeatParticleShader(std::map<GLenum, std::string> defines = std::map<GLenum, std::string>{});
 
-	virtual bool createProgram(std::map<GLenum, std::string> defines = std::map<GLenum, std::string> defines{});
+	virtual bool createProgram(std::map<GLenum, std::string> defines = std::map<GLenum, std::string>{});
 
 	void draw(const renderlib::Camera &cam, const renderlib::Drawable &obj);
 };
