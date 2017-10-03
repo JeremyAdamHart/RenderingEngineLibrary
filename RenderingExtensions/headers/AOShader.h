53847a98230e78b2fc2e933c7080c2faef9173a5
JeremyAdamHart <jeremyadamhart@gmail.com>
2017-10-02 18:07:23 -0600

    Sorry Andrew

--- RenderingExtensions/headers/AOShader.h
+++ RenderingExtensions/headers/AOShader.h
@@ -19,9 +19,9 @@ protected:
 	void loadUniforms(glm::vec3 camera_pos, glm::vec3 cam_right, 
 		glm::vec3 cam_up, glm::vec3 light_pos);
 public:
-	AOShader(map<GLenum, string> defines = {});
+	AOShader(map<GLenum, string> defines = map<GLenum, string>{});
 
-	virtual bool createProgram(map<GLenum, string> defines = {});
+	virtual bool createProgram(map<GLenum, string> defines = map<GLenum, string>{});
 
 	void draw(const Camera &cam, glm::vec3 lightPos, const Drawable &obj);		//Change to use pointer to drawable?
 	void draw(const Camera &cam, glm::vec3 lightPos, Scene &scene);
